e0cf2f03980b3254a74e9230e1a3ded3bbea985d
Kohshi Yamaguchi <kohshi54.yam@gmail.com>
2025-04-07 05:30:27 +0900

    read body for Content-Length bytes

--- spicy-http/src/my_http___linker__.cc
+++ spicy-http/src/my_http___linker__.cc
@@ -11,13 +11,18 @@
 #include <spicy/rt/libspicy.h>
 
 namespace __hlt_my_http::MyHTTP {
+    struct FieldLine;
     struct HTTPRequest;
     extern void __hook_HTTPRequest___on_0x25_done(::hilti::rt::ValueReference<HTTPRequest>& __self);
-    extern void __hook_HTTPRequest___on_0x25_done_e70e(::hilti::rt::ValueReference<HTTPRequest>& __self);
+    extern void __hook_HTTPRequest___on_0x25_done_6199(::hilti::rt::ValueReference<HTTPRequest>& __self);
+    extern void __hook_HTTPRequest___on_headers_foreach(const ::hilti::rt::ValueReference<__hlt_my_http::MyHTTP::FieldLine>& __dd, ::hilti::rt::Bool& __stop, ::hilti::rt::ValueReference<HTTPRequest>& __self);
+    extern void __hook_HTTPRequest___on_headers_foreach_41a2(const ::hilti::rt::ValueReference<__hlt_my_http::MyHTTP::FieldLine>& __dd, ::hilti::rt::Bool& __stop, ::hilti::rt::ValueReference<HTTPRequest>& __self);
 }
 
 const char HILTI_EXPORT HILTI_WEAK * __hlt_my_http_hlto_library_version = R"({"debug":false,"hilti_version":10804,"magic":"v1"})";
 const char HILTI_EXPORT HILTI_WEAK * __hlt_my_http_hlto_bind_to_version = spicy_version_1_8_4();
-const char HILTI_WEAK * __hlt_my_http_hlto_scope = "a23ca65d67e521b0";
+const char HILTI_WEAK * __hlt_my_http_hlto_scope = "f3c2a0d637b03df4";
 
-extern void __hlt_my_http::MyHTTP::__hook_HTTPRequest___on_0x25_done(::hilti::rt::ValueReference<HTTPRequest>& __self) { __hlt_my_http::MyHTTP::__hook_HTTPRequest___on_0x25_done_e70e(__self); }
+extern void __hlt_my_http::MyHTTP::__hook_HTTPRequest___on_0x25_done(::hilti::rt::ValueReference<HTTPRequest>& __self) { __hlt_my_http::MyHTTP::__hook_HTTPRequest___on_0x25_done_6199(__self); }
+
+extern void __hlt_my_http::MyHTTP::__hook_HTTPRequest___on_headers_foreach(const ::hilti::rt::ValueReference<__hlt_my_http::MyHTTP::FieldLine>& __dd, ::hilti::rt::Bool& __stop, ::hilti::rt::ValueReference<HTTPRequest>& __self) { __hlt_my_http::MyHTTP::__hook_HTTPRequest___on_headers_foreach_41a2(__dd, __stop, __self); }
--- spicy-http/test/test.cpp
+++ spicy-http/test/test.cpp
@@ -41,15 +41,52 @@ TEST_F(SpicyHTTPTest, parse_ok_test2) {
 	EXPECT_EQ(buffer.str(), "GET, /index.html, 1.1\nHost:example.com\n");
 }
 
+// Body with Content-Length should be ignored
 TEST_F(SpicyHTTPTest, parse_ok_test3) {
 	std::istringstream testInput("GET /index.html HTTP/1.1\r\nHost: example.com\r\n\r\nhello=world");
 	std::stringstream buffer;
 	std::streambuf* oldCoutBuffer = std::cout.rdbuf(buffer.rdbuf());
 	SpicyHTTPParser(testInput);
 	std::cout.rdbuf(oldCoutBuffer);
-	EXPECT_EQ(buffer.str(), "GET, /index.html, 1.1\nHost:example.com\nhello=world\n");
+	EXPECT_EQ(buffer.str(), "GET, /index.html, 1.1\nHost:example.com\n");
 }
 
+TEST_F(SpicyHTTPTest, parse_with_content_length1) {
+	std::istringstream testInput("GET / HTTP/1.1\r\nHost: example.com\r\nContent-Length: 11\r\n\r\nhello=world");
+	std::stringstream buffer;
+	std::streambuf* oldCoutBuffer = std::cout.rdbuf(buffer.rdbuf());
+	SpicyHTTPParser(testInput);
+	std::cout.rdbuf(oldCoutBuffer);
+	EXPECT_EQ(buffer.str(), 
+			"GET, /, 1.1\n"
+			"Host:example.com\n"
+			"Content-Length:11\n"
+			"hello=world\n");
+}
+
+// Content-Length is less than actual body size => Only pecified Content-Length is read
+TEST_F(SpicyHTTPTest, parse_with_content_length2) {
+	std::istringstream testInput("GET / HTTP/1.1\r\nHost: example.com\r\nContent-Length: 8\r\n\r\nhello=world");
+	std::stringstream buffer;
+	std::streambuf* oldCoutBuffer = std::cout.rdbuf(buffer.rdbuf());
+	SpicyHTTPParser(testInput);
+	std::cout.rdbuf(oldCoutBuffer);
+	EXPECT_EQ(buffer.str(), 
+			"GET, /, 1.1\n"
+			"Host:example.com\n"
+			"Content-Length:8\n"
+			"hello=wo\n");
+}
+
+// Content-Length is larger than actual body size => The message is considered to be incompolete
+TEST_F(SpicyHTTPTest, parse_with_content_length3) {
+	std::istringstream testInput("GET / HTTP/1.1\r\nHost: example.com\r\nContent-Length: 15\r\n\r\nhello=world");
+	std::stringstream buffer;
+	std::streambuf* oldCerrBuffer = std::cerr.rdbuf(buffer.rdbuf());
+	SpicyHTTPParser(testInput);
+	std::cerr.rdbuf(oldCerrBuffer);
+	EXPECT_EQ(buffer.str(), "parse error\n");
+}
 
 int main(int argc, char **argv) {
 	hilti::rt::init();
